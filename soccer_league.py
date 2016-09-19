ccdb401a99187b28352691e89bd742768e4bbe8a
tangamasinqule <masinqule.tanga@gmail.com>
2016-09-19 08:26:29 +0200

    cleaned up

--- soccer_league.py
+++ soccer_league.py
@@ -1,39 +1,48 @@
 #!/usr/bin/env python
 from __future__ import print_function
-import argparse
-import os
 import logging
 import click
-log = logging.getLogger( __name__ )
+import codecs
+
+log = logging.getLogger(__name__)
+
+"""Click allows to handle file input with the benefit of
+   dealing with Unicode and bytes correctly for all versions of Python so my script stays very portable.
+   http://click.pocoo.org/5/arguments/
+"""
 
 @click.command()
-@click.option('--input_file', type=click.Path(exists=True), help='The input file with the team name and scores of the league games.')
+@click.option('--input_file', type=click.Path(exists=True),
+              help='The input file with the team name and scores of the league games.')
 @click.option('--output_file', type=click.Path(exists=False), help='The output file with each team game statistics.')
 def main(input_file, output_file):
     lines = [line.rstrip('\n') for line in open(input_file)]
     team_set = build_team_list(lines)
 
+    #build the league results dictionary
     league = {}
     for team in team_set:
         league[team] = 0
         for l in lines:
             if team in l:
-                league[team] = league[team] + calculate_points(team, l)
+                league[team] += calculate_points(team, l)
 
-    #sort the dictionary by value
-    sorted_list = [(k,v) for v,k in sorted(
-                 [(v,k) for k,v in league.items()], reverse=True
-                 )
-              ]
+    # sorting the dictionary by value
+    sorted_list = [(k, v) for v, k in sorted(
+            [(v, k) for k, v in league.items()], reverse=True
+    )
+                   ]
     write_output_file(sorted_list, output_file)
+
 
 def build_team_list(teams):
     team_list = []
     for t in teams:
         x = t.split(',')
-        team_list.append(x[0][:-1].strip())
-        team_list.append(x[1][:-1].strip())
+        team_list.append(x[0].rsplit(' ', 1)[0].strip())
+        team_list.append(x[1].rsplit(' ', 1)[0].strip())
     return set(team_list)
+
 
 def calculate_points(team, score_line):
     scores = score_line.split(',')
@@ -54,13 +63,14 @@ def calculate_points(team, score_line):
         points = 0
     return points
 
+
 def write_output_file(sorted_list, output_file):
-    f = open(output_file,'w')
+    f = codecs.open(output_file, encoding='utf-8', mode='w+')
     for sl in sorted_list:
         line = str(list(sl)[0]) + ", " + str(list(sl)[1]) + " pts"
-        f.write(line + '\n') # python will convert \n to os.linesep
+        f.write(line + '\n')
     f.close()
+
 
 if __name__ == '__main__':
     main()
-
